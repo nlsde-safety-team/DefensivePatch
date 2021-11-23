ccad66db8a6d928459d08d1b838abec7d67c7eee
Yzx835 <46290866+Yzx835@users.noreply.github.com>
2021-11-23 22:23:40 +0800

    update

--- fusion_style.py
+++ fusion_style.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from torchvision import transforms
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as F
 
 from torchvision import datasets, transforms, models
 import torchvision
@@ -12,10 +13,10 @@ import numpy as np
 import os
 import tqdm
 from PIL import Image
-import vgg
-import resnet
-import mobilenet
-import shufflenet
+import models.vgg as vgg
+import models.resnet as resnet
+import models.mobilenet as mobilenet
+import models.shufflenet as shufflenet
 
 import json
 import pickle
@@ -46,7 +47,6 @@ log_dir = make_log_dir()
 
 
 def pad_transform(patch, image_size, patch_size, offset):
-    # print(offset)
     offset_x, offset_y = offset
 
     pad = torch.nn.ConstantPad2d((offset_x - patch_size // 2, image_size- patch_size - offset_x + patch_size // 2, offset_y - patch_size // 2, image_size-patch_size-offset_y + patch_size // 2), 0) #left, right, top ,bottom
@@ -101,7 +101,6 @@ def initialize_model(model_name):
         model = resnet.resnet50(num_classes=10).cuda()
         model.load_state_dict(torch.load('finetunes/resnet50_best_new.pth'))
         
-
     else:
         print("Invalid model name, exiting...")
         exit()
@@ -109,21 +108,6 @@ def initialize_model(model_name):
     return model
 
 
-def gram_matrix(input):
-    a, b, c, d = input.shape
-    features = input.view(a, b, c * d)
-    assert features[0][0][1] == features.transpose(1, 2)[0][1][0]
-
-    
-    G = torch.matmul(features, features.transpose(1, 2)) / b
-    return G
-
-features = []
-def get_viz(module, inputs):
-#     print(len(inputs))
-#     print(inputs[0].shape)
-    features.append(inputs[0])
-    
 
 transform = transforms.Compose([transforms.ToTensor(),
                           transforms.Normalize((0.5,), (0.5,)),
@@ -138,17 +122,13 @@ print(len(testset))
 train_loader = DataLoader(trainset, batch_size=32, shuffle=True, num_workers=8)
 test_loader = DataLoader(testset, batch_size=32, shuffle=False, num_workers=8)
 
-cirterion1 = torch.nn.CrossEntropyLoss()
-cirterion2 = torch.nn.MSELoss()
-
 prior = torch.randn((10, 3, 32, 32)).cuda()
 prior.data /= 100
 prior.requires_grad_(True)
 
 prior_mask = torch.zeros((1, 1, 32, 32)).cuda()
-prior_mask[:, :, 12:20, 12:20] = 1
-# prior_mask[:, :, 1:31, 1:31] = 1
-# prior_mask[:, :, 3:29, 3:29] = 0
+prior_mask[:, :, 1:31, 1:31] = 1
+prior_mask[:, :, 3:29, 3:29] = 0
 
 torchvision.utils.save_image(prior_mask, 'mask.png', nrow=1)
 
@@ -161,13 +141,6 @@ print(model)
 for param in model.parameters():
     param.requires_grad = False
 
-count = 0
-for name, m in model.named_modules():
-    if isinstance(m, torch.nn.Conv2d):
-        print(count, name)
-        if count in [9]:
-            m.register_forward_pre_hook(get_viz)
-        count += 1
     
 model.cuda()
 model.eval()
@@ -179,37 +152,27 @@ for epoch in range(10):
     
     correct = 0
     total = 0
-    prob = 0
+    _prob = 0
     
     tqdm_bar = tqdm.tqdm(train_loader)
     for i, data in enumerate(tqdm_bar):
         inputs, labels = data
         inputs = inputs.cuda()
         labels = labels.cuda()
-        
-#         prior_inputs, mask = pad_transform(prior, IMAGE_SIZE, PATCHSIZE, (IMAGE_SIZE//2, IMAGE_SIZE//2))
+        batchsize = labels.size(0)
 
         
         prior_inputs = prior.index_select(0, labels) * prior_mask
-        # mask = 
-#         prior_inputs = prior_inputs * mask
-#         print(viz_inputs.shape)
 
-        features = []
 
         model(inputs)
         outputs = model(prior_inputs)
 
-        loss1 = cirterion1(outputs, labels)
-        loss2 = 0
-        _L = len(features) // 2
-        for l in range(_L):
-            
-            gram_1 = gram_matrix(features[l])
-            gram_2 = gram_matrix(features[l + _L])
-            loss2 += cirterion2(gram_1, gram_2)
+        prob = F.softmax(outputs, dim=1)
+        prob_index = prob.index_select(1, labels)
+        loss1 = (-torch.log(prob_index + 1e-10) * torch.eye(batchsize).cuda()).sum() / batchsize
 
-        loss = loss1 # + loss2
+        loss = loss1 
 
         optimizer.zero_grad()
         loss.backward()
@@ -218,14 +181,14 @@ for epoch in range(10):
         predicted = torch.max(outputs, 1)[1]
         correct += (predicted == labels).sum()
 
-        probs = torch.nn.functional.softmax(outputs)
+        probs = torch.nn.functional.softmax(outputs, dim=1)
         for _ in range(labels.shape[0]):
-            prob += probs[_, labels[_]]
+            _prob += probs[_, labels[_]]
 
 
         total += labels.shape[0]
-
-        tqdm_bar.set_description('acc %.4f, conf %.4f, loss1 %.4f, loss2 %.4f' % (correct.float()/total, prob/total, loss1, loss2))
+ 
+        tqdm_bar.set_description('acc %.4f, conf %.4f, loss %.4f' % (correct.float()/total, _prob/total, loss))
         
         if i % 50 == 0:
             save_image(prior, os.path.join(log_dir, 'prior.png'))
--- train_large_patch_multipatch.py
+++ train_large_patch_multipatch.py
@@ -12,15 +12,13 @@ import copy
 import tqdm
 import time
 import copy
-import iv3
-import resnet
+import models.resnet as resnet
 import torchvision.transforms.functional as TF
-# from dataloader import mydataset, mydataset_imagenet
 import argparse
-import vgg
-import mobilenet
-import shufflenet
-import alex
+import models.vgg as vgg
+import models.mobilenet as mobilenet
+import models.shufflenet as shufflenet
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--trans", type=str, default='')
 parser.add_argument("--target", type=int, default=None)
@@ -53,7 +51,6 @@ def make_log_dir():
         'loss': 'new_gram_patch',
         'prior': args.prior + '_C_mask',
         'lamb': args.lamb,
-        # 'gram': '3',
         'target_index': TARGET_INDEX,
     }
     dir_name = ""
@@ -71,14 +68,12 @@ log_dir = make_log_dir()
 def save_image(image_tensor, save_file):
     image_tensor = image_tensor.clone()
     image_tensor = image_tensor[:16]
-    # print(image_tensor.shape)
     image_tensor[:, 0, :, :] = image_tensor[:, 0, :, :] * 0.5 + 0.5 
     image_tensor[:, 1, :, :] = image_tensor[:, 1, :, :] * 0.5 + 0.5 
     image_tensor[:, 2, :, :] = image_tensor[:, 2, :, :] * 0.5 + 0.5 
     torchvision.utils.save_image(image_tensor, save_file, nrow=4)
 
 def pad_transform(patch, image_size, patch_size, offset):
-    # print(offset)
     offset_x, offset_y = offset
 
     pad = torch.nn.ConstantPad2d((offset_x - patch_size // 2, image_size- patch_size - offset_x + patch_size // 2, offset_y - patch_size // 2, image_size-patch_size-offset_y + patch_size // 2), 0) #left, right, top ,bottom
@@ -132,13 +127,6 @@ def initialize_model(model_name):
         """
         model = vgg.vgg16_bn(num_classes=10).cuda()
         model.load_state_dict(torch.load('finetunes/vgg16_best_new.pth'))
-    elif model_name == "vgg16_pat":
-        """ VGG16
-        """
-        model = vgg.vgg16_bn(num_classes=10).cuda()
-        model = nn.DataParallel(model)
-        model.load_state_dict(torch.load('/root/yinzixin/defense_patch/defense_PAT/adv-plat/experiments/cifar_vgg16_bn_PAT_new/ckpt/model_final.pth'))
-        model = model.module
 
     elif model_name == "vgg19":
         """ VGG19
@@ -149,40 +137,16 @@ def initialize_model(model_name):
     elif model_name == "mobilenet":
         model = mobilenet.MobileNetV2(10).cuda()
         model.load_state_dict(torch.load('finetunes/mobilenet_best_new.pth'))
-    elif model_name == "mobilenet_pat":
-        model = mobilenet.MobileNetV2(10).cuda()
-        model = nn.DataParallel(model)
-        model.load_state_dict(torch.load('/root/yinzixin/defense_patch/defense_PAT/adv-plat/experiments/cifar_mobilenet_PAT_new/ckpt/model_final.pth'))
-        model = model.module
 
     elif model_name == "shufflenet":
         model = shufflenet.ShuffleNet(10).cuda()
         model.load_state_dict(torch.load('finetunes/shufflenet_best_new.pth'))
-    elif model_name == "shufflenet_pat":
-        model = shufflenet.ShuffleNet(10).cuda()
-        model = nn.DataParallel(model)
-        model.load_state_dict(torch.load('/root/yinzixin/defense_patch/defense_PAT/adv-plat/experiments/cifar_shufflenet_PAT_new/ckpt/model_final.pth'))
-        model = model.module
 
     elif model_name == "resnet50":
         """ Resnet50
         """
         model = resnet.resnet50(num_classes=10).cuda()
         model.load_state_dict(torch.load('finetunes/resnet50_best_new.pth'))
-    elif model_name == "resnet50_pat":
-        """ Resnet50
-        """
-        model = resnet.resnet50(num_classes=10).cuda()
-        model = nn.DataParallel(model)
-        model.load_state_dict(torch.load('/root/yinzixin/defense_patch/defense_PAT/adv-plat/experiments/cifar_resnet50_PAT_new/ckpt/model_final.pth'))
-        model = model.module
-
-    elif model_name == "alexnet":
-        """ Alexnet
-        """
-        model = alex.alexnet(num_classes=10).cuda()
-        model.load_state_dict(torch.load('finetunes/alexnet_best_2.pth'))
-        
 
     else:
         print("Invalid model name, exiting...")
@@ -215,13 +179,9 @@ def gen_mask(image_size, patch_size, offset, center):
 
 
 prior_mask = gen_mask(IMAGE_SIZE, PATCHSIZE, (IMAGE_SIZE//2, IMAGE_SIZE//2), PATCHCENTER)
-# [1, 32, 32]
 print(prior_mask.shape)
-# change the shape but do not increase or decrease the number of the elements
 prior_mask = prior_mask.view(prior_mask.shape[0], -1)
-# [1, 1024]
-# print(prior_mask.shape)
-# all >1 position
+
 indexs = []
 for idx in range(prior_mask.shape[1]):
     if prior_mask[0, idx] > 0:
@@ -240,40 +200,11 @@ def new_gram(fs):
     _fs = torch.matmul(_patch.transpose(1, 2), _image)
     return _fs
 
-def PCA(fs, k=10):
-    batch, channel, H, W = fs.shape
-    X = fs.view(batch, channel, H * W)
-    X_mean = X.mean(dim=(1,2), keepdim=True)
-    X_mean = X_mean.repeat((1, channel, H*W))
-    X = X - X_mean.expand_as(X)
-    
-    X_t = X.transpose(1, 2)
-    U, S, V = torch.svd(X_t)
-        
-    X_PCA = torch.matmul(X, U[:, :, :k])
-        
     return X_PCA
 
 lamb = [float(x) for x in args.lamb.split('-')]
 print(lamb)
 def loss_feature(fs, labels):
-    # loss = 0
-    # _L = len(fs) // 2
-    # # print(len(fs))
-    # for i in range(_L):
-    #     # print(fs[i].shape)
-    #     f1 = gram(fs[i][1])
-    #     f2 = gram(fs[i + _L][1])
-    #     print(f1.shape)
-    #     if len(f1.shape) == 4:
-    #         total = f1.shape[0] * f1.shape[1] * f1.shape[2] * f1.shape[3]
-    #     elif len(f1.shape) == 3:
-    #         total = f1.shape[0] * f1.shape[1] * f1.shape[2]
-
-        
-    #     _loss = torch.pow(f1 - f2, 2).sum() # / total
-    #     loss += _loss * lamb[i]
-    
     new_loss = 0
     _L = len(fs) // 2
     for i in range(_L):
@@ -289,11 +220,6 @@ def loss_feature(fs, labels):
         _loss = torch.pow(f1 - f2, 2).sum()  / total
         new_loss += _loss * lamb[i]
 
-    
-    # print(loss)
-    # print(new_loss)
-    # assert loss == new_loss
-    # print(_L)
     return new_loss / _L
 
 
@@ -313,10 +239,6 @@ print(len(testset))
 train_loader = DataLoader(trainset, batch_size=32, shuffle=True, num_workers=8)
 test_loader = DataLoader(testset, batch_size=32, shuffle=False, num_workers=8)
 
-
-
-# save_image(defense_patch, 'raw_patch.png')
-
 def train_patch(defense_patch, model):
     global features
 
@@ -324,6 +246,7 @@ def train_patch(defense_patch, model):
     defense_patch.requires_grad_(True)
 
     optimizer = torch.optim.Adam([defense_patch], lr=0.03)#, weight_decay=1e-4)
+    cirterion = torch.nn.CrossEntropyLoss()
 
     for epoch in range(1):
         
@@ -350,8 +273,8 @@ def train_patch(defense_patch, model):
             prob_index = prob.index_select(1, labels)
 
             
-            loss_f = 0 # cirterion(outputs_patch, labels)
-            loss_p = (-torch.log(prob_index + 1e-10) * torch.eye(batchsize).to(device)).sum() / batchsize
+            loss_f = 0 
+            loss_p = cirterion(outputs, labels)
             loss = loss_f + loss_p
             
             optimizer.zero_grad()
@@ -386,40 +309,9 @@ for model_name in model_names:
     models.append(model)
 
 
-# model_1 = initialize_model('resnet50')
-# for param in model_1.parameters():
-#     param.requires_grad = False
-# model_1.to(device)
-# model_1.eval()
-# count = 0
-# for name, m in model_1.named_modules():
-# #     print(name)
-#     if isinstance(m, torch.nn.Conv2d):
-#         # print(count, name)
-#         if count in [46]:
-#             print(name)
-#             m.register_forward_pre_hook(viz_collection)
-#         count += 1
-
-
-# model_2 = initialize_model('alexnet')
-# for param in model_2.parameters():
-#     param.requires_grad = False
-# # print(model_2)
-# model_2.to(device)
-# model_2.eval()
-# count = 0
-# for name, m in model_2.named_modules():
-# #     print(name)
-#     if isinstance(m, torch.nn.Conv2d):
-#         # print(count, name)
-#         if count in [9]:
-#             print(name)
-#             m.register_forward_pre_hook(viz_collection)
-#         count += 1
 
 if args.prior != 'None':
-    defense_patch = torch.load('new_logs/fusion-%s+epoch-5+layer-9+loss-CE_mask+/prior.pkl' % (args.prior))
+    defense_patch = torch.load('test_logs/fusion-%s+epoch-5+shape-30-26+loss-CE_mask+/prior.pkl' % (args.prior))
     defense_patch = defense_patch[:, :, 1:31, 1:31]
     defense_patch = torch.autograd.Variable(defense_patch).to(device)
 else:
@@ -430,7 +322,6 @@ print(defense_patch.shape)
 optimizer_defense_patch = torch.optim.Adam([defense_patch], lr=0.03)#, weight_decay=1e-4)
 scheduler_defense_patch = torch.optim.lr_scheduler.MultiStepLR(optimizer_defense_patch, milestones=[3, 8, 15, 25, 40, 60, 90, 140], gamma=1/3)
 
-
 best_epoch = 0
 best_acc = 0
 for epoch in range(30):
@@ -439,8 +330,6 @@ for epoch in range(30):
     for model in models:
         defense_patch_ = train_patch(defense_patch=defense_patch.clone(), model=model)
         defense_patch_s.append(defense_patch_)
-    # defense_patch_1 = train_patch(defense_patch=defense_patch.clone(), model=model_1)
-    # defense_patch_2 = train_patch(defense_patch=defense_patch.clone(), model=model_2)
 
     total = 0
     correct = 0
@@ -471,45 +360,18 @@ for epoch in range(30):
 
             prob = F.softmax(model_outputs, dim=1)
             prob_index = prob.index_select(1, labels)
-            loss_p += 0 # (-torch.log(prob_index + 1e-10) * torch.eye(batchsize).to(device)).sum() / batchsize
-
             model_outputs_s.append(model_outputs)
 
-        # defense_inputs_1 = get_inputs(inputs, labels, defense_patch_1)
-        # defense_inputs_2 = get_inputs(inputs, labels, defense_patch_2)
-
-        # features = []
-        # model_1_output_defense_inputs = model_1(defense_inputs)
-        # model_2_output_defense_inputs = model_2(defense_inputs)
-        # model_1_output_defense_inputs_1 = model_1(defense_inputs_1)
-        # model_2_output_defense_inputs_2 = model_2(defense_inputs_2)
 
         features = []
         for idx in range(len(defense_inputs_s)):
             features.append((defense_new_patch, defense_inputs))
         for idx in range(len(defense_inputs_s)):
             features.append((defense_new_patch_s[idx], defense_inputs_s[idx]))
-            
-        
-        # features.append(defense_inputs)
-        # features.append(defense_inputs)
-        # features.append(defense_inputs_1)
-        # features.append(defense_inputs_2)
 
         loss_f = loss_feature(features, labels)
 
-
-        # prob_1 = F.softmax(model_1_output_defense_inputs, dim=1)
-        # prob_index_1 = prob_1.index_select(1, labels)
-
-        # prob_2 = F.softmax(model_2_output_defense_inputs, dim=1)
-        # prob_index_2 = prob_2.index_select(1, labels)
-
-        # loss_p = (-torch.log(prob_index_1 + 1e-10) * torch.eye(batchsize).to(device)).sum() / batchsize + \
-        #          (-torch.log(prob_index_2 + 1e-10) * torch.eye(batchsize).to(device)).sum() / batchsize
-
-
-        loss = loss_f # + loss_p
+        loss = loss_f
         optimizer_defense_patch.zero_grad()
         loss.backward()
         optimizer_defense_patch.step()
@@ -517,12 +379,9 @@ for epoch in range(30):
         for model_outputs in model_outputs_s:
             predicted = torch.max(model_outputs, 1)[1]
             correct += (predicted == labels).sum().float() / len(models)
-        # predicted = torch.max(model_2_output_defense_inputs, 1)[1]
-        # correct += (predicted == labels).sum().float()
+
         total += labels.size(0)
         total_loss += loss
-        
-        # print(loss)
         
         if i % 100 == 0:
             output1 = defense_inputs
